728041c46ec3ab80ac194691bba72a46d745746f
My PC <My PC@192.168.5.73>
2025-09-23 11:15:07 +0800

    Initial commit

---
+++ rectangle/src/rectangle/RectangeRunner.java
@@ -0,0 +1,14 @@
+package rectangle;
+
+public class RectangeRunner {
+
+	public static void main(String[] args) {
+		Rectangle rectangle = new Rectangle(12, 23);
+		System.out.println(rectangle);
+		rectangle.setWidth(22);
+		System.out.println(rectangle);
+		rectangle.setLength(23);
+		System.out.println(rectangle);
+	}
+
+}
